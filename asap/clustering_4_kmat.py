7e366cfc4eda63ea1c01a2de8d8ca22104843777
BingqingCheng <tonicbq@gmail.com>
2019-08-12 17:53:11 +0100

    changed clustering tools

--- asap/clustering_4_kmat.py
+++ asap/clustering_4_kmat.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from lib import kpca, kerneltorho, kerneltodis
 from lib import get_cluster_size, get_cluster_properties
-from sklearn.cluster import DBSCAN
+from lib import DBCluster, sklearn_DB
 
-def main(fkmat, ftags, fcolor, prefix, kpca_d, pc1, pc2):
+def main(fkmat, ftags, prefix, kpca_d, pc1, pc2):
 
     # if it has been computed before we can simply load it
     try:
@@ -22,55 +22,40 @@ def main(fkmat, ftags, fcolor, prefix, kpca_d, pc1, pc2):
 
     # charecteristic difference in k_ij
     sigma_kij = np.std(eva[:,:])
-
     # do a low dimensional projection to visualize the data
     proj = kpca(eva,kpca_d)
 
-    # #############################################################################
-    # simple clustering schemes using DBSCAN
-
-    # Generate sample data
-    X = proj
-    # Compute DBSCAN
-    veps = sigma_kij
-    vminsamples = 5
-    db = DBSCAN(eps=veps, min_samples=vminsamples).fit(X)
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
-    labels_db = db.labels_
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels_db)) - (1 if -1 in labels_db else 0)
-    n_noise_ = list(labels_db).count(-1)
-
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Estimated number of noise points: %d' % n_noise_)
-    #############################################################################
+    # now we do the clustering
+    # option 1: do on the projected coordinates
+    #trainer = sklearn_DB('euclidean')
+    #do_clustering = DBCluster(sigma_kij, 5, trainer)
+    #do_clustering.fit(proj)
+    # option 2: do directly on kernel matrix.
+    dmat = kerneltodis(eva)
+    trainer = sklearn_DB('precomputed')
+    do_clustering = DBCluster(sigma_kij, 5, trainer)
+    do_clustering.fit(dmat)
+    #
+    labels_db = do_clustering.get_cluster_labels()
+    n_clusters = do_clustering.get_n_cluster()
 
     # save
-    #np.savetxt(prefix+"-kpca-d"+str(kpca_d)+".coord", proj, fmt='%4.8f')
+    np.savetxt(prefix+"-cluster-label.dat", labels_db, fmt='%d')
 
-    [ unique_labels, cluster_size ]  = get_cluster_size(labels_db[ndict:])
+    [ unique_labels, cluster_size ]  = get_cluster_size(labels_db[:])
     # center of each cluster
     [ unique_labels, cluster_x ]  = get_cluster_properties(labels_db[:],proj[:,pc1],'mean')
     [ unique_labels, cluster_y ]  = get_cluster_properties(labels_db[:],proj[:,pc2],'mean')
 
     # color scheme
-    if (fcolor != 'none'):
-        try:
-            plotcolor = np.genfromtxt(fcolor, dtype=float)
-        except: raise ValueError('Cannot load the vector of properties')
-        if (len(plotcolor) != len(eva)): 
-            raise ValueError('Length of the vector of properties is not the same as number of samples')
-        colorlabel = 'use '+fcolor+' for coloring the data points'
-    else: # we use the local density as the color scheme
-        plotcolor = kerneltorho(eva, sigma_kij)
-        colorlabel = 'local density of each data point ($\sigma(k_{ij})$ ='+"{:4.0e}".format(sigma_kij)+' )'
-    [ plotcolormin, plotcolormax ] = [ np.min(plotcolor),np.max(plotcolor) ]
+    plotcolor = labels_db
+    [ plotcolormin, plotcolormax ] = [ 0, n_clusters ]
+    colorlabel = 'a total of' + str(n_clusters) + ' clusters'
 
     # make plot
     fig, ax = plt.subplots()
     pcaplot = ax.scatter(proj[:,pc1],proj[:,pc2],c=plotcolor[:],
-                    cmap=cm.summer,vmin=plotcolormin, vmax=plotcolormax)
+                    cmap=cm.gnuplot,vmin=plotcolormin, vmax=plotcolormax)
     cbar = fig.colorbar(pcaplot, ax=ax)
     cbar.ax.set_ylabel(colorlabel)
 
@@ -79,17 +64,6 @@ def main(fkmat, ftags, fcolor, prefix, kpca_d, pc1, pc2):
         if (k >=0):
             ax.plot(cluster_x[k],cluster_y[k], 'o', markerfacecolor='none',
                 markeredgecolor='gray', markersize=10.0*(np.log(cluster_size[k])))
-
-
-    # the noisy points
-    if k == -1:
-        # Black removed and is used for noise instead.
-        # Black used for noise.
-        col = [0, 0, 0, 1]
-        class_member_mask = (labels_db == k)
-        xy = X[class_member_mask & ~core_samples_mask]
-        ax.plot(xy[:, pc1], xy[:, pc2], 'o', markerfacecolor='gray',
-             markeredgecolor='k', markersize=1.5)
 
     # project the known structures
     if (ftags != 'none'):
@@ -111,13 +85,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-kmat', type=str, required=True, help='Location of kernel matrix file. You can use gen_kmat.py to compute it.')
     parser.add_argument('-tags', type=str, default='none', help='Location of tags for the first M samples')
-    parser.add_argument('-colors', type=str, default='none', help='Properties for all samples (N floats) used to color the scatter plot')
-    parser.add_argument('--prefix', type=str, default='', help='Filename prefix')
+    parser.add_argument('--prefix', type=str, default='ASAP', help='Filename prefix')
     parser.add_argument('--d', type=int, default=10, help='number of the principle components to keep')
     parser.add_argument('--pc1', type=int, default=0, help='Plot the projection along which principle axes')
     parser.add_argument('--pc2', type=int, default=1, help='Plot the projection along which principle axes')
     args = parser.parse_args()
 
-    main(args.kmat, args.tags, args.colors, args.prefix, args.d, args.pc1, args.pc2)
+    main(args.kmat, args.tags, args.prefix, args.d, args.pc1, args.pc2)
 
 
