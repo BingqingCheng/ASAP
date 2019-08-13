101cfb59705dba45b45e902d9f4f5f769f17805d
BingqingCheng <tonicbq@gmail.com>
2019-08-13 10:46:15 +0100

    added examples

--- examples/Ni-icosahedral-np/test.sh
+++ examples/Ni-icosahedral-np/test.sh
@@ -1,8 +1,11 @@
 # step 1
-../../asap/gen_kmat.py -fxyz Ni-icosa.xyz --prefix Ni-icosa --rcut 4 --n 6 --l 6 --g 0.5 --periodic True --plot True
+../../asap/gen_soap_kmat.py -fxyz Ni-icosa.xyz --prefix Ni-icosa --rcut 4 --n 6 --l 6 --g 0.5 --periodic True --plot True
 
-# step 2
+# step 2.1
 ../../asap/kpca.py -kmat Ni-icosa-n6-l6-c4.0-g0.5.kmat --prefix Ni-icosa --d 10 --pc1 0 --pc2 1
+
+# step 2.2
+../../asap/kernel_density_estimation.py -kmat Ni-icosa-n6-l6-c4.0-g0.5.kmat --prefix Ni-icosa --pc1 0 --pc2 1
 
 # step 3
 ../../asap/clustering.py -kmat Ni-icosa-n6-l6-c4.0-g0.5.kmat --prefix Ni-icosa
