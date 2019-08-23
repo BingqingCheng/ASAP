5cb5a1928b31c9b56c6a811133670d0fb5de560b
BingqingCheng <tonicbq@gmail.com>
2019-08-23 11:56:42 +0100

    add adjusttext function

--- asap/kpca.py
+++ asap/kpca.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from asaplib.pca import kpca
 from asaplib.plot import plot_styles
+from asaplib.io import str2bool
 
-def main(fkmat, ftags, fcolor, prefix, kpca_d, pc1, pc2):
+def main(fkmat, ftags, fcolor, prefix, kpca_d, pc1, pc2, adtext):
 
     # if it has been computed before we can simply load it
     try:
@@ -46,7 +47,7 @@ def main(fkmat, ftags, fcolor, prefix, kpca_d, pc1, pc2):
                 xlabel='Princple Axis '+str(pc1), ylabel='Princple Axis '+str(pc2), 
                 clabel=colorlabel, label=None,
                 centers=None,
-                psize=20,
+                psize=500,
                 out_file='KPCA_4_'+prefix+'.png', 
                 title='KPCA for: '+prefix, 
                 show=False, cmap='summer',
@@ -57,10 +58,22 @@ def main(fkmat, ftags, fcolor, prefix, kpca_d, pc1, pc2):
                 vmax = plotcolormax,
                 vmin = plotcolormin)
 
+    fig.set_size_inches(18.5, 10.5)
+
     if (ftags != 'none'):
+        texts = []
         for i in range(ndict):
             ax.scatter(proj[i,pc1],proj[i,pc2],marker='^',c='black')
-            ax.annotate(tags[i], (proj[i,pc1], proj[i,pc2]))
+            texts.append(ax.text(proj[i,pc1],proj[i,pc2], tags[i],
+                         ha='center', va='center', fontsize=15,color='red'))
+            #ax.annotate(tags[i], (proj[i,pc1], proj[i,pc2]))
+        if (adtext):
+            from adjustText import adjust_text
+            adjust_text(texts,on_basemap=True,# only_move={'points':'', 'text':'x'},
+                    expand_text=(1.01, 1.05), expand_points=(1.01, 1.05),
+                   force_text=(0.03, 0.5), force_points=(0.01, 0.25),
+                   ax=ax, precision=0.001,
+                  arrowprops=dict(arrowstyle="-", color='black', lw=1,alpha=0.8))
 
     plt.show()
     fig.savefig('KPCA_4_'+prefix+'.png')
@@ -78,8 +91,10 @@ if __name__ == '__main__':
     parser.add_argument('--d', type=int, default=10, help='number of the principle components to keep')
     parser.add_argument('--pc1', type=int, default=0, help='Plot the projection along which principle axes')
     parser.add_argument('--pc2', type=int, default=1, help='Plot the projection along which principle axes')
+    parser.add_argument('--adjusttext', type=str2bool, nargs='?', const=True, default=False, help='Do you want to adjust the texts (True/False)?')
+
     args = parser.parse_args()
 
-    main(args.kmat, args.tags, args.colors, args.prefix, args.d, args.pc1, args.pc2)
+    main(args.kmat, args.tags, args.colors, args.prefix, args.d, args.pc1, args.pc2, args.adjusttext)
 
 
