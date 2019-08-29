a6a4b30b33db261dee13a2f5b2af3a9e138afe1b
BingqingCheng <tonicbq@gmail.com>
2019-08-29 17:05:13 +0100

    added new example

--- examples/singlet-triplet-fission/test.sh
+++ examples/singlet-triplet-fission/test.sh
@@ -1,3 +1,3 @@
-../../asap/gen_kmat.py -fxyz all.xyz --prefix test --rcut 4 --n 6 --l 6 --g 0.5 --periodic False --plot True
+../../asap/gen_kmat.py -fxyz all.xyz --prefix test --rcut 2.5 --n 4 --l 6 --g 0.4 --periodic False --plot True
 
-../../asap/kpca_4_kmat.py -kmat test-n6-l6-c4.0-g0.5.kmat --prefix test --d 10 --pc1 0 --pc2 1
+../../asap/kpca.py -kmat test-n4-l6-c2.5-g0.4.kmat --prefix test --d 10 --pc1 0 --pc2 1
