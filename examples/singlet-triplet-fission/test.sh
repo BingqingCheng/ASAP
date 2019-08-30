b1cf577e1caf3fb038b222e1ca04faa06851cffb
Bingqing Cheng <tonicbq@gmail.com>
2019-08-30 18:40:27 +0100

    Delete test.sh

--- examples/singlet-triplet-fission/test.sh
+++
@@ -1,3 +0,0 @@
-../../asap/gen_kmat.py -fxyz subset.xyz --prefix subset --rcut 2.5 --n 4 --l 6 --g 0.4 --periodic False --plot True
-
-../../asap/kpca.py -kmat subset-n4-l6-c2.5-g0.4.kmat --prefix subset --d 10 --pc1 0 --pc2 1
