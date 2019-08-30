809801de3afa15569adf7a9ab2eb7fa84bb69e5b
Bingqing Cheng <tonicbq@gmail.com>
2019-08-30 18:40:10 +0100

    Delete test.sh

--- examples/water-nnp-trainset/test.sh
+++
@@ -1,9 +0,0 @@
-
-# step 1
-../../asap/gen_soap_kmat.py -fxyz dataset_1593_eVAng.xyz --prefix H2O --rcut 4.0 --n 4 --l 6 --g 0.3 --periodic True --plot True
-
-# step 2
-../../asap/kernel_density_estimation.py -kmat H2O-n4-l6-c4.0-g0.3.kmat --pc1 0 --pc2 1
-
-# clustering
-../../asap/clustering.py -kmat H2O-n4-l6-c4.0-g0.3.kmat
