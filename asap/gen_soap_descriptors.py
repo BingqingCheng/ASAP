820668fb69997752a98bdbb75a6b0750ef74088b
BingqingCheng <tonicbq@gmail.com>
2019-11-07 10:17:09 +0000

    clean up

--- asap/gen_soap_descriptors.py
+++ asap/gen_soap_descriptors.py
@@ -53,48 +53,28 @@ def main(fxyz, dictxyz, prefix, output, peratom, soap_rcut, soap_g, soap_n, soap
     global_species = np.unique(global_species)
     print("a total of", nframes, "frames, with elements: ", global_species)
 
-    if nframes > 1:
-        # set up the soap descriptors
-        soap_desc = SOAP(species=global_species, rcut=soap_rcut, nmax=soap_n, lmax=soap_l,
-                         sigma=soap_g, crossover=False, average=True, periodic=soap_periodic)
-    else:
-        # if only one frame we compute the soap descriptors for each atomic environment
-        # within this frame
-        soap_desc = SOAP(species=global_species, rcut=soap_rcut, nmax=soap_n, lmax=soap_l,
+    soap_desc_atomic = SOAP(species=global_species, rcut=soap_rcut, nmax=soap_n, lmax=soap_l,
                          sigma=soap_g, crossover=False, average=False, periodic=soap_periodic)
 
-    # compute soap finger prints
-    fall = soap_desc.create(frames, n_jobs=8)
+    for i, frame in enumerate(frames):
+        fnow = soap_desc_atomic.create(frame, n_jobs=8)
 
-    fshape = np.shape(fall)
-    print("the shape of the soap descriptor matrix: ", fshape)
+        # average over all atomic environments inside the system
+        frame.info['soap_desc'] = fnow.mean(axis=0)
 
-    # save
-    if output == 'matrix':
-        np.savetxt(prefix+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)+".desc",
-               fall, fmt='%4.8f')
-    elif output == 'xyz':
-        if peratom: # !!!!! this will be changed to avoid the double computation !!!!!!        
-            soap_desc_atomic = SOAP(species=global_species, rcut=soap_rcut, nmax=soap_n, lmax=soap_l,
-                         sigma=soap_g, crossover=False, average=False, periodic=soap_periodic)
-        if nframes > 1:
-            for i, frame in enumerate(frames):
-                frame.info['soap_desc'] = fall[i]
-                # will change!!!
-                if peratom:
-                    fnow = soap_desc_atomic.create(frame, n_jobs=8)
-                    frame.new_array('soap_desc', fnow)
-                #  will change
-                write(prefix+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)+".xyz",
+        # save
+        if output == 'matrix':
+            with open(prefix+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)+".desc", "ab") as f:
+                np.savetxt(f, frame.info['soap_desc'])
+        elif output == 'xyz':
+            # output per-atom info
+            if peratom:
+                frame.new_array('soap_desc', fnow)
+            # write xyze
+            write(prefix+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)+".xyz",
                  frame, append=True)
         else:
-            frames[0].new_array('soap_desc', fall)
-            write(prefix+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)+".xyz",
-                 frames[0], append=False)
-
-    else:
-        raise ValueError('Cannot find the output format')
-
+            raise ValueError('Cannot find the output format')
 
 if __name__ == '__main__':
 
@@ -103,7 +83,7 @@ if __name__ == '__main__':
     parser.add_argument('-fdict', type=str, default='none', help='Location of xyz file '
                                                                  'that is used for a dictionary')
     parser.add_argument('--prefix', type=str, default='ASAP', help='Filename prefix')
-    parser.add_argument('--output', type=str, default='matrix', help='The format for output files ([xyz], [matrix])')
+    parser.add_argument('--output', type=str, default='xyz', help='The format for output files ([xyz], [matrix])')
     parser.add_argument('--peratom', type=str2bool, nargs='?', const=True, default=False,
                         help='Do you want to output per atom descriptors for multiple frames (True/False)?')
     parser.add_argument('--rcut', type=float, default=3.0, help='Cutoff radius')
