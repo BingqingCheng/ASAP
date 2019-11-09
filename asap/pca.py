73b0abf2d9971c7f2046beae9864d07a8951bb8f
BingqingCheng <tonicbq@gmail.com>
2019-11-09 11:34:34 +0000

    much improved error handling and logic

--- asap/pca.py
+++ asap/pca.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import argparse
-import os
+import sys,os
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from asaplib.pca import pca, pca_project
@@ -10,9 +10,11 @@ from asaplib.plot import *
 from asaplib.io import str2bool
 from ase.io import read,write
 
-def main(fmat, fxyz, ftags, fcolor, colorscol, prefix, output, peratom, scale, pca_d, pc1, pc2, adtext):
+def main(fmat, fxyz, ftags, fcolor, colorscol, prefix, output, peratom, keepraw, scale, pca_d, pc1, pc2, adtext):
 
+    foutput = prefix+"-pca-d"+str(pca_d)
     peratom = bool(peratom)
+    keepraw = bool(keepraw)
 
     # if a descriptor matrix has been computed before we can simply load it
     if os.path.isfile(fmat):
@@ -43,24 +45,16 @@ def main(fmat, fxyz, ftags, fcolor, colorscol, prefix, output, peratom, scale, p
                          if ( ndesc > 0 and len(frame.info[fmat]) != ndesc): raise ValueError('mismatch of number of descriptors between frames')
                          ndesc = len(frame.info[fmat])
                      except:
-                         try: 
-                             desc.append(frame.info['soap_desc'])
-                             if ( ndesc > 0 and len(frame.info[fmat]) != ndesc): raise ValueError('mismatch of number of descriptors between frames')
-                             ndesc = len(frame.info[fmat])
-                         except:
-                             raise ValueError('Cannot combine the descriptor matrix from the xyz file')
-                else: raise ValueError('Cannot load the descriptor matrix from from frame'+str(i)+' of the xyz file')
+                         raise ValueError('Cannot combine the descriptor matrix from the xyz file')
+                else: raise ValueError('Cannot load the descriptor matrix from from frame No. '+str(i)+' of the xyz file')
 
             desc = np.asmatrix(desc)
             desc.reshape((ndesc,nframes))
-
         else:
             # only one frame
             try: 
                 desc = frames[0].get_array(fmat)
-            except: 
-                try: frames[0].get_array('soap_desc')
-                except: ValueError('Cannot read the descriptor matrix from single frame')
+            except: ValueError('Cannot read the descriptor matrix from single frame')
 
     if ftags != 'none':
         tags = np.loadtxt(ftags, dtype="str")
@@ -78,8 +72,9 @@ def main(fmat, fxyz, ftags, fcolor, colorscol, prefix, output, peratom, scale, p
 
     # save
     if output == 'matrix':
-        np.savetxt(prefix+"-pca-d"+str(pca_d)+".coord", proj, fmt='%4.8f', header='low D coordinates of samples')
+        np.savetxt(foutput+".coord", proj, fmt='%4.8f', header='low D coordinates of samples')
     elif output == 'xyz':
+        if os.path.isfile(foutput+".xyz"): os.rename(foutput+".xyz","bck."+foutput+".xyz")
         if nframes > 1:
             for i, frame in enumerate(frames):
                 frame.info['pca_coord'] = proj[i]
@@ -87,16 +82,16 @@ def main(fmat, fxyz, ftags, fcolor, colorscol, prefix, output, peratom, scale, p
                 if peratom:
                     try: 
                         desc_atomic = frame.get_array(fmat)
-                    except: 
-                        try: desc_atomic = frame.get_array('soap_desc')
-                        except: ValueError('Cannot read the descriptor per atom for frame '+str(i))
+                    except: ValueError('Cannot read the descriptor per atom for frame '+str(i))
                     desc_atomic = scaler.transform(desc_atomic) # normalizing the features
-                    #print(np.shape(desc_atomic),np.shape(pvec))
                     proj_atomic = pca_project(desc_atomic, pvec)
                     frame.new_array('pca_coord', proj_atomic)
-                # !!! this is the bits for per_atom proj
-                #print(frame)
-                write(prefix+"-pca-d"+str(pca_d)+".xyz",frame, append=True)
+                # remove the raw descriptors
+                if not keepraw:
+                    frame.info[fmat] = None
+                    frame.set_array(fmat,None)
+
+                write(foutput+".xyz",frame, append=True)
         else:
             frames[0].new_array('pca_coord', proj)
             write(prefix+"-pca-d"+str(pca_d)+".xyz",frames[0], append=False)
@@ -147,23 +142,28 @@ def main(fmat, fxyz, ftags, fcolor, colorscol, prefix, output, peratom, scale, p
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-fmat', type=str, required=True, help='Location of descriptor matrix file or name of the tags in ase xyz file. You can use gen_descriptors.py to compute it.')
+    parser.add_argument('-fmat', type=str, default='ASAP_desc', help='Location of descriptor matrix file or name of the tags in ase xyz file. You can use gen_descriptors.py to compute it.')
     parser.add_argument('-fxyz', type=str, default='none', help='Location of xyz file for reading the properties.')
-    parser.add_argument('-tags', type=str, default='none', help='Location of tags for the first M samples')
-    parser.add_argument('-colors', type=str, default='none', help='Location of a file that contains properties for all samples (N floats) used to color the scatter plot')
+    parser.add_argument('-tags', type=str, default='none', help='Location of tags for the first M samples. Plot the tags on the PCA map.')
+    parser.add_argument('-colors', type=str, default='none', help='Location of a file or name of the tags in ase xyz file. It should contain properties for all samples (N floats) used to color the scatter plot')
     parser.add_argument('--colorscolumn', type=int, default=0, help='The column number of the properties used for the coloring. Starts from 0.')
     parser.add_argument('--prefix', type=str, default='ASAP', help='Filename prefix')
     parser.add_argument('--output', type=str, default='xyz', help='The format for output files ([xyz], [matrix])')
     parser.add_argument('--peratom', type=str2bool, nargs='?', const=True, default=False,
                         help='Do you want to output per atom pca coordinates (True/False)?')
+    parser.add_argument('--keepraw', type=str2bool, nargs='?', const=True, default=False,
+                        help='Do you want to keep the high dimensional descriptor when output xyz file (True/False)?')
     parser.add_argument('--scale', type=str2bool, nargs='?', const=True, default=True, help='Scale the coordinates (True/False). Scaling highly recommanded.')
     parser.add_argument('--d', type=int, default=10, help='number of the principle components to keep')
     parser.add_argument('--pc1', type=int, default=0, help='Plot the projection along which principle axes')
     parser.add_argument('--pc2', type=int, default=1, help='Plot the projection along which principle axes')
     parser.add_argument('--adjusttext', type=str2bool, nargs='?', const=True, default=False, help='Do you want to adjust the texts (True/False)?')
 
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
     args = parser.parse_args()
 
-    main(args.fmat, args.fxyz, args.tags, args.colors, args.colorscolumn, args.prefix, args.output, args.peratom, args.scale, args.d, args.pc1, args.pc2, args.adjusttext)
+    main(args.fmat, args.fxyz, args.tags, args.colors, args.colorscolumn, args.prefix, args.output, args.peratom, args.keepraw, args.scale, args.d, args.pc1, args.pc2, args.adjusttext)
 
 
