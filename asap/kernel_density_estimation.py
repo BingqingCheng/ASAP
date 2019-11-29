410007eaf3bdd857c4302faf3f04ac4a4390aa95
BingqingCheng <tonicbq@gmail.com>
2019-11-29 18:01:08 +0000

    added ase reader

--- asap/kernel_density_estimation.py
+++ asap/kernel_density_estimation.py
@@ -1,34 +1,66 @@
 #!/usr/bin/python3
 
 import argparse
-
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from asaplib.kde import KDE
 from asaplib.plot import plot_styles
 from asaplib.io import str2bool
+from ase.io import read
 
 
-def main(fmat, ftags, prefix, dimension, pc1, pc2, adtext):
+def main(fmat, fxyz, ftags, prefix, dimension, pc1, pc2, adtext):
 
-    # if it has been computed before we can simply load it
-    try:
-        proj = np.genfromtxt(fmat, dtype=float)[:,0:dimension]
-    except:
-        raise ValueError('Cannot load the coordinates')
-    print("loaded", fmat)
+    # try to read the xyz file
+    if fxyz != 'none':
+        try:
+            frames = read(fxyz,':')
+            nframes = len(frames)
+            print('load xyz file: ',fxyz, ', a total of ', str(nframes), 'frames')
+        except: 
+            raise ValueError('Cannot load the xyz file')
+
+        desc = []; ndesc = 0
+        # load from xyze file
+        if nframes > 1:
+            for i, frame in enumerate(frames):
+                if fmat in frame.info:
+                     try:
+                         desc.append(frame.info[fmat])
+                         if ( ndesc > 0 and len(frame.info[fmat]) != ndesc): raise ValueError('mismatch of number of descriptors between frames')
+                         ndesc = len(frame.info[fmat])
+                     except:
+                         raise ValueError('Cannot combine the descriptor matrix from the xyz file')
+        else:
+            # only one frame
+            try: 
+                desc = frames[0].get_array(fmat)
+            except: ValueError('Cannot read the descriptor matrix from single frame')
+    # we can also load the descriptor matrix from a standalone file
+    if os.path.isfile(fmat):
+        try:
+            desc = np.genfromtxt(fmat, dtype=float)
+            print("loaded the descriptor matrix from file: ", fmat)
+        except:
+            raise ValueError('Cannot load the descriptor matrix from file')
+    if len(desc)==0: raise ValueError('Please supply descriptor in a xyz file or a standlone descriptor matrix')
+    print("loaded", fmat, " with shape", np.shape(desc))
     # load tags if any
     if ftags != 'none':
         tags = np.loadtxt(ftags, dtype="str")
         ndict = len(tags)
 
+    
+    proj = np.asmatrix(desc)[:,0:dimension]
     density_model = KDE()        
     # fit density model to data
     try:
         density_model.fit(proj)
     except:
-        raise RuntimeError('KDE did not work. Try smaller d.')   
+        raise RuntimeError('KDE did not work. Try smaller d.')  
+
     sigma_kij = density_model.bandwidth
     rho = density_model.evaluate_density(proj)
     # save the density
@@ -42,7 +74,7 @@ def main(fmat, ftags, prefix, dimension, pc1, pc2, adtext):
     # make plot
     plot_styles.set_nice_font()
     # density plot
-    fig, ax = plot_styles.plot_density_map(proj[:,[pc1,pc2]], plotcolor,
+    fig, ax = plot_styles.plot_density_map(np.asarray(proj[:,[pc1,pc2]]), plotcolor,
                 xlabel='Princple Axis '+str(pc1), ylabel='Princple Axis '+str(pc2), 
                 clabel=colorlabel, label=None,
                 centers=None,
@@ -80,7 +112,8 @@ def main(fmat, ftags, prefix, dimension, pc1, pc2, adtext):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-fmat', type=str, required=True, help='Location of low dimensional coordinate file.')
+    parser.add_argument('-fmat', type=str, required=True, help='Location of low dimensional coordinate file or name of the tags in ase xyz file.')
+    parser.add_argument('-fxyz', type=str, default='none', help='Location of xyz file for reading the properties.')
     parser.add_argument('-tags', type=str, default='none', help='Location of tags for the first M samples')
     parser.add_argument('--prefix', type=str, default='ASAP', help='Filename prefix')
     parser.add_argument('--d', type=int, default=10, help='number of the first X dimensions to keep')
@@ -89,6 +122,6 @@ if __name__ == '__main__':
     parser.add_argument('--adjusttext', type=str2bool, nargs='?', const=True, default=False, help='Do you want to adjust the texts (True/False)?')
     args = parser.parse_args()
 
-    main(args.fmat, args.tags, args.prefix, args.d, args.pc1, args.pc2, args.adjusttext)
+    main(args.fmat, args.fxyz, args.tags, args.prefix, args.d, args.pc1, args.pc2, args.adjusttext)
 
 
