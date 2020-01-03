b34ae419a05f85c7359602e1a6db54b0739696dd
BingqingCheng <tonicbq@gmail.com>
2020-01-03 16:45:14 +0000

    added option to use multiple soap

--- asap/gen_soap_descriptors.py
+++ asap/gen_soap_descriptors.py
@@ -8,7 +8,7 @@ from dscribe.descriptors import SOAP
 from asaplib.io import str2bool
 
 
-def main(fxyz, dictxyz, prefix, output, peratom, soap_rcut, soap_g, soap_n, soap_l, soap_periodic):
+def main(fxyz, dictxyz, prefix, output, peratom, fmultisoap, soap_rcut, soap_g, soap_n, soap_l, soap_periodic):
     """
 
     Generate the SOAP descriptors.
@@ -19,14 +19,13 @@ def main(fxyz, dictxyz, prefix, output, peratom, soap_rcut, soap_g, soap_n, soap
     dictxyz: string giving location of xyz file that is used as a dictionary
     prefix: string giving the filename prefix
     output: [xyz]: append the SOAP descriptors to extended xyz file; [mat] output as a standlone matrix
+    fmultisoap: use multiple sets of SOAP descriptors using parameters specified in fmultisoap file (json format)
     soap_rcut: float giving the cutoff radius, default value is 3.0
     soap_g: float giving the atom width
     soap_n: int giving the maximum radial label
     soap_l: int giving the maximum angular label. Must be less than or equal to 9
     soap_periodic: string (True or False) indicating whether the system is periodic
     """
-    foutput = prefix+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)
-    desc_name = "SOAP"+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)
     soap_periodic = bool(soap_periodic)
     peratom = bool(peratom)
     fframes = []
@@ -54,16 +53,38 @@ def main(fxyz, dictxyz, prefix, output, peratom, soap_rcut, soap_g, soap_n, soap
     global_species = np.unique(global_species)
     print("a total of", nframes, "frames, with elements: ", global_species)
 
-    soap_desc_atomic = SOAP(species=global_species, rcut=soap_rcut, nmax=soap_n, lmax=soap_l,
-                         sigma=soap_g, rbf="gto", crossover=False, average=False, periodic=soap_periodic)
+    if fmultisoap == 'none':
+        soap_desc_atomic = [SOAP(species=global_species, rcut=soap_rcut, nmax=soap_n, lmax=soap_l,
+                         sigma=soap_g, rbf="gto", crossover=False, average=False, periodic=soap_periodic)]
+        foutput = prefix+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)
+        desc_name = "SOAP"+"-n"+str(soap_n)+"-l"+str(soap_l)+"-c"+str(soap_rcut)+"-g"+str(soap_g)
+    else:
+        import json
+        try: 
+            with open(fmultisoap, 'r') as soapfile: soap_js=json.load(soapfile)
+        except:
+            raise IOError('Cannot load the json file for soap parameters')
+        soap_desc_atomic = []
+        for element in soap_js.keys():
+            soap_param = soap_js[element]
+            [ species_now, cutoff_now, g_now, n_now, l_now ] = [ soap_param['species'], soap_param['cutoff'], soap_param['atom_gaussian_width'], soap_param['n'],soap_param['l']]
+            soap_desc_atomic.append(SOAP(species=species_now, rcut=cutoff_now, nmax=n_now, lmax=l_now,
+                         sigma=g_now, rbf="gto", crossover=False, average=False, periodic=soap_periodic))
+
+        foutput = prefix+"-multisoap"+'-'+fmultisoap
+        desc_name = "MULTISOAP"+'-'+fmultisoap
+
 
     # prepare for the output
     if os.path.isfile(foutput+".xyz"): os.rename(foutput+".xyz","bck."+foutput+".xyz")
     if os.path.isfile(foutput+".desc"): os.rename(foutput+".desc","bck."+foutput+".desc")
 
     for i, frame in enumerate(frames):
-        fnow = soap_desc_atomic.create(frame, n_jobs=8)
-
+        fnow = soap_desc_atomic[0].create(frame, n_jobs=8)
+        #print(np.shape(fnow))
+        for soap_desc_atomic_now in soap_desc_atomic[1:]:
+            fnow = np.append(fnow,soap_desc_atomic_now.create(frame, n_jobs=8),axis=1)
+            #print(np.shape(fnow))
         # average over all atomic environments inside the system
         frame.info[desc_name] = fnow.mean(axis=0)
 
@@ -93,6 +114,7 @@ if __name__ == '__main__':
     parser.add_argument('--output', type=str, default='xyz', help='The format for output files ([xyz], [matrix])')
     parser.add_argument('--peratom', type=str2bool, nargs='?', const=True, default=False,
                         help='Do you want to output per atom descriptors for multiple frames (True/False)?')
+    parser.add_argument('-multisoap', type=str, default='none', help='Use multiple SOAP, and specify the parameter using a json file.')
     parser.add_argument('--rcut', type=float, default=3.0, help='Cutoff radius')
     parser.add_argument('--n', type=int, default=6, help='Maximum radial label')
     parser.add_argument('--l', type=int, default=6, help='Maximum angular label (<= 9)')
@@ -106,4 +128,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args = parser.parse_args()
 
-    main(args.fxyz, args.fdict, args.prefix, args.output, args.peratom, args.rcut, args.g, args.n, args.l, args.periodic)
+    main(args.fxyz, args.fdict, args.prefix, args.output, args.peratom, args.multisoap, args.rcut, args.g, args.n, args.l, args.periodic)
