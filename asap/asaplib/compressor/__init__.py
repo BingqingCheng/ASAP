1d2d0af391cd9bcf583597d1f082f598492ade8c
BingqingCheng <tonicbq@gmail.com>
2020-01-28 19:02:01 +0000

    added CUR selection for input data

--- asap/asaplib/compressor/__init__.py
+++ asap/asaplib/compressor/__init__.py
@@ -1,3 +1,3 @@
 from .fps import *
 from .split import *
-
+from .cur import CUR_deterministic
--- asap/asaplib/compressor/cur.py
+++ asap/asaplib/compressor/cur.py
@@ -7,42 +7,48 @@ import scipy.linalg as salg
 import scipy.sparse.linalg as spalg
 
 
-def cCURSelOrtho(cov, numSym, costs=1):
-    """ Apply (deterministic) CUR selection of numSymm rows & columns of the
+def CUR_deterministic(X, n_col, error_estimate=True, costs=1):
+
+    """
+    Given rank k, find the best column and rows
+    X = C U R
+
+    Parameters
+    ----------
+    X: input a covariance matrix 
+    n_col: number of column to keep
+    error_estimate: compute the remaining error of the CUR
+    costs: a list of costs associated with each column
+
+    Returns
+    -------
+    a list of selected columns
+    """
+
+    RX = X.copy()
+    rsel = np.zeros(n_col, dtype=int)
+    rerror = np.zeros(n_col, dtype=float)
+    
+    for i in range(n_col):
+        rval, RX = CUR_deterministic_step(RX, n_col-i, costs)
+        rsel[i] = rval
+        if error_estimate: rerror[i] = np.sum(np.abs(RX))
+    return rsel, rerror
+
+
+def CUR_deterministic_step(cov, k, costs=1):
+    """ Apply (deterministic) CUR selection of k rows & columns of the
     given covariance matrix, including an orthogonalization step. Costs can be weighted if desired. """
 
-    evc,evec = spalg.eigs(cov,numSym)
+    evc, evec = spalg.eigs(cov, k)
+    # compute the weights and select the one with maximum weight
     weights = np.sum(np.square(evec),axis=1)/costs
     sel = np.argmax(weights)
     vsel = cov[sel].copy()
     vsel *= 1.0/np.sqrt(np.dot(vsel,vsel))
-    ocov = cov.copy()
-    for i in xrange(len(ocov)):
+    rcov = cov.copy()
+    for i in range(len(rcov)):
         # Diagonalize the covariance matrix wrt the chosen column
-        ocov[i] -= vsel * np.dot(cov[i],vsel)
+        rcov[i] -= vsel * np.dot(cov[i],vsel)
 
-    return sel, ocov
-
-
-def CURSelOrtho(cov, numSym, costs=1):
-
-    """
-
-    Parameters
-    ----------
-    cov
-    numSym
-    costs
-
-    Returns
-    -------
-
-    """
-
-    ocov = cov.copy()
-    rsel = np.zeros(numSym, int)
-
-    for i in xrange(numSym):
-        rval, ocov = cCURSelOrtho(ocov, numSym-i, costs)
-        rsel[i] = rval
-    return rsel
+    return sel, rcov
