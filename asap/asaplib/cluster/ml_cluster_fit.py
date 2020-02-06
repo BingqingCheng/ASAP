d9e9ad8ec7216575c79fcd270218b7ed232011bc
BingqingCheng <tonicbq@gmail.com>
2020-02-06 19:50:59 +0000

    add documentation for Laio_db

--- asap/asaplib/cluster/ml_cluster_fit.py
+++ asap/asaplib/cluster/ml_cluster_fit.py
@@ -12,7 +12,7 @@ import numpy as np
 
 class DBCluster(ClusterBase):
     """
-    TODO: Make class-level docstring
+    Performing clustering using density based clustering algorithm
     """
     _pairwise = True
 
@@ -108,11 +108,28 @@ class LAIO_DB(FitClusterBase):
 
     $$ \rho_i\,=\,\sum_j{\chi(d_{ij}-d_{cut})}$$
     $$ \delta_i\,=\,\min_{j:\rho_j>\rho_i}(d_{ij})$$
+
+    A summary of laio clustering algorithm:
+    1. First do a kernel density estimation (rho_i) for each data point i 
+    2. For each data point i, compute the distance (delta_i) between i and j, 
+       j is the closet data point that has a density higher then i, i.e. rho(j) > rho(i).
+    3. Plot the decision graph, which is a scatter plot of (rho_i, delta_i)
+    4. Select cluster centers ({cl}), which are the outliers in the decision graph that fulfills:
+       i) rho({cl}) > rhomin
+       ii) delta({cl}) > delta_min
+       one needs to set the two parameters rhomin and delta_min.
+    5. After the cluster centers are determined, data points are assigned to the nearest cluster center.
+ one needs to set two parameters:
     """
 
     _pairwise = True
 
     def __init__(self, deltamin=-1, rhomin=-1):
+        """
+        rhomin: the lower bound in kernel density, if the density of a cluster center is 
+                lower than this threshold, this "cluster" will be discarded as noise
+        deltamin: the lower bound on the distance of two cluster center.
+        """
         self.deltamin = deltamin
         self.rhomin = rhomin
 
@@ -127,7 +144,7 @@ class LAIO_DB(FitClusterBase):
 
         Returns
         -------
-
+        cluster label
         """
 
         if rho is None:
@@ -135,13 +152,17 @@ class LAIO_DB(FitClusterBase):
 
         delta, nneigh = self.estimate_delta(dmatrix, rho)
 
-        # I'll think about this!!!
+        # if there's no input values for rhomin and deltamin, we use simple heuristics
         if self.rhomin < 0:
             self.rhomin = 0.2*np.mean(rho)+0.8*np.min(rho)
         if self.deltamin < 0:
             self.deltamin = np.mean(delta)
 
-        ###
+        """
+        here we make the decision graph
+        x axis (rho) is the kernel density for each data point
+        y axis (delta) is the distance to the nearest higher density points
+        """
         plt.scatter(rho, delta)
         plt.plot([min(rho), max(rho)], [self.deltamin, self.deltamin], c='red')
         plt.plot([self.rhomin, self.rhomin], [min(delta), max(delta)], c='red')
@@ -149,9 +170,11 @@ class LAIO_DB(FitClusterBase):
         plt.ylabel('delta')
         plt.show()
         ###
+
         nclust = 0
         cl = np.zeros(len(rho), dtype='int')-1
         for i in range(len(rho)):
+            # we select cluster centers {cl} with rho({c}) > rhomin && delta({c}) > delta_min
             if rho[i] > self.rhomin and delta[i] > self.deltamin:
                 nclust += 1
                 cl[i] = nclust
@@ -165,14 +188,20 @@ class LAIO_DB(FitClusterBase):
         return cl
 
     def estimate_delta(self, dist, rho):
+    ''' For each data point i, compute the distance (delta_i) between i and j, 
+       j is the closet data point that has a density higher then i, i.e. rho(j) > rho(i).
+    '''
         delta = (rho*0.0).copy()
         nneigh = np.ones(len(delta), dtype='int')
         for i in range(len(rho)):
+            # for data i, find all points that have higher density
             js = np.where(rho > rho[i])[0]
             if len(js) == 0:
+                # if there's no j's that have higher density than i, we set delta_i to be a large distance
                 delta[i] = np.max(dist[i, :])
                 nneigh[i] = i
             else:
+                # find the nearest j that has higher density then i
                 delta[i] = np.min(dist[i, js])
                 nneigh[i] = js[np.argmin(dist[i, js])]
         return delta, nneigh
