9aca54421b8f700ca752e382d58233cf8daafabd
Tamas K. Stenczel <stenczelt@gmail.com>
2020-03-24 14:35:19 +0000

    tests updated to pytest
    still able to run them simply with `make` from the test dir

--- tests/run_all.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import os
-
-import asaplib
-print('Successfully imported asaplib')
-
-# find tests and run them
-suite = unittest.defaultTestLoader.discover(os.getcwd())
-unittest.TextTestRunner().run(suite)
