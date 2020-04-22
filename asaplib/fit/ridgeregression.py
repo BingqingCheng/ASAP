d39c7556b4751e29896a165e1b9a5d37f90ca41d
BingqingCheng <tonicbq@gmail.com>
2020-04-22 14:38:49 +0100

    clean up

--- asaplib/fit/ridgeregression.py
+++ asaplib/fit/ridgeregression.py
@@ -39,7 +39,16 @@ class RidgeRegression(RegressorBase):
         '''kernel.shape is expected as (nPred, nTrain)'''
         return np.dot(desc, self.alpha.flatten()).reshape((-1))
 
-    def geterror(self, desc, y):
+    def get_error(self, desc, y):
+        """
+        compute the score of the predictions compared with the true values
+        Parameters
+        ----------
+        desc : array-like, shape=[n_descriptors, n_samples]
+        Input points.
+        y : array-like, shape=[n_samples]
+        Input points.
+        """
         # get the predictions for train set
         y_pred = self.predict(desc)
         # compute the CV score
@@ -49,12 +58,12 @@ class RidgeRegression(RegressorBase):
     def get_train_test_error(self, desc_train, y_train, desc_test, y_test, verbose=True, return_pred=True):
         fit_error = {}
         # compute the CV score for the train dataset
-        train_error, y_pred = self.geterror(desc_train, y_train)
+        train_error, y_pred = self.get_error(desc_train, y_train)
         if verbose: print("train score: ", train_error)
         fit_error['train_error'] = train_error
 
         # compute the CV score for the test dataset
-        test_error, y_pred_test = self.geterror(desc_test, y_test)
+        test_error, y_pred_test = self.get_error(desc_test, y_test)
         if verbose: print("test score: ", test_error)
         fit_error['test_error'] = test_error
 
