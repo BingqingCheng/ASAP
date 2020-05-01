1f2a9c1586ff859e6e5b1ae867987028412495c3
BingqingCheng <tonicbq@gmail.com>
2020-05-01 09:06:35 +0100

    added option to output atomic coord

--- scripts/pca.py
+++ scripts/pca.py
@@ -77,6 +77,7 @@ def main(fmat, fxyz, ftags, fcolor, colorscol, prefix, output, peratom, keepraw,
     # save
     if output == 'matrix':
         np.savetxt(foutput + ".coord", proj, fmt='%4.8f', header='low D coordinates of samples')
+        if peratom:  np.savetxt(foutput + "-atomic.coord", proj, fmt='%4.8f', header='low D coordinates of samples')
     if output == 'xyz':
         if os.path.isfile(foutput + ".xyz"):
             os.rename(foutput + ".xyz", "bck." + foutput + ".xyz")
